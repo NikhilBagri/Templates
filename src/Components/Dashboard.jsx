384ed56fd0b9480701a3084041913a804d2f4c31
Nikhil <nikhilbagri18@gmail.com>
2023-03-20 14:42:56 +0530

    new2

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,24 +1,44 @@
 import React from "react";
-import './Dashboard.css'
+import "./Dashboard.css";
 
 function Dashboard() {
   return (
     <div className="container">
-        <div class="row">
-          <div className="card mx-auto my-3" style={{ width: "600px" }}>
-            <img className="card-img-top" src="Templates\Blue.jpg" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Template blue</h5>
-            </div>
+      <h1>Dashboard</h1>
+      <div class="row">
+        <div className="card mx-auto my-3" style={{ width: "600px" }}>
+          <img className="card-img-top" src="Templates\Blue.jpg" alt="Red" />
+          <div className="card-body">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault1"
+            />
+            <h5 className="card-title">Template Blue</h5>
+            <label class="form-check-label" for="flexRadioDefault1"> 
+            </label>
           </div>
-          <div className="card mx-auto my-3" style={{ width: "600px" , height: "400px"}}>
-            <img className="card-img-top" src="Templates\Red.jpg" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Template Red</h5>
-            </div>
+        </div>
+        <div
+          className="card mx-auto my-3"
+          style={{ width: "600px", height: "400px" }}
+        >
+          <img className="card-img-top" src="Templates\Red.jpg" alt="Blue" />
+          <div className="card-body">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault1"
+            />
+            <h5 className="card-title">Template Red</h5>
+            <label class="form-check-label" for="flexRadioDefault1"> 
+            </label>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,30 +3,24 @@ import "./Home.css";
 export default function Home() {
   return (
     <>
-    <h1>Login to select a template</h1>
       <div className="container">
+      <h1>Login to select a template</h1>
         <div class="row">
           <div className="card mx-auto my-3" style={{ width: "600px" }}>
-            <img className="card-img-top" src="Templates\Blue.jpg" alt="Card image cap" />
+            <img className="card-img-top" src="Templates\Blue.jpg" alt="Blue" />
             <div className="card-body">
               <h5 className="card-title">Template blue</h5>
             </div>
           </div>
           <div className="card mx-auto my-3" style={{ width: "600px" , height: "400px"}}>
-            <img className="card-img-top" src="Templates\Red.jpg" alt="Card image cap" />
+            <img className="card-img-top" src="Templates\Red.jpg" alt="Red" />
             <div className="card-body">
               <h5 className="card-title">Template Red</h5>
             </div>
           </div>
         </div>
       </div>
-      <div className="d-flex flex-column justify-content-center w-100 h-100">
-        <div className="d-flex flex-column justify-content-center align-items-center">
-          <div className="btn-group my-5">
-            <a href="https://codepen-api-export-production.s3.us-west-2.amazonaws.com/zip/PEN/pyBNzX/1578778289271/pure-css-gradient-background-animation.zip" />
-          </div>
-        </div>
-      </div>
+     
     </>
   );
 }
